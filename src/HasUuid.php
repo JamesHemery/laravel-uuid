83a5ae48ba41d6ce086577a1fd0bd25849139e86
James <jameshemery.pro@gmail.com>
2019-04-27 16:41:37 +0200

    HasUuid Trait

---
+++ src/HasUuid.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Jamesh\Uuid;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * Trait HasUuid
+ * @package Jamesh\Uuid
+ */
+trait HasUuid
+{
+
+    /**
+     * Used by Eloquent to get primary key type.
+     * UUID Identified as a string.
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    /**
+     * Used by Eloquent to get if the primary key is auto increment value.
+     * UUID is not.
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Add behavior to creating and saving Eloquent events.
+     * @return void
+     */
+    public static function bootHasUuid()
+    {
+        // Create a UUID to the model if it does not have one
+        static::creating(function ($model) {
+            $model->keyType = 'string';
+            $model->incrementing = false;
+
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string)Str::uuid();
+            }
+        });
+
+        // Set original if someone try to change UUID on update/save existing model
+        static::saving(function (Model $model) {
+            $original_id = $model->getOriginal('id');
+            if ($original_id !== $model->id) {
+                $model->id = $original_id;
+            }
+        });
+    }
+}
